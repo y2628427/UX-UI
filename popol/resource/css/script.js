b23ce89aa7a50acb664c81081abddeee536c009c
woo jong <dnwhdtlstls@naver.com>
2021-06-02 13:36:29 +0900

    popol

--- popol/resource/css/script.js
+++ popol/resource/css/script.js
@@ -1,11 +1,8 @@
 $(function () {
-    $(".slider").bxSlider({
-        captions: true,
-        auto:false,
-        slideWidth:220,
-        pause:1000,
-        onSlideNext:function(d){
-            console.log(d);
-        }
+    $(".slider_1").bxSlider({
+        maxSlides: 3,
+        slideWidth: 220,
+        paper: true,
+        slideMargin: 15
     });
 });

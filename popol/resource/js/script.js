955d20b64a40d54927b4dc4e02937be3a12ecf4f
junhee <x2628427@naver.com>
2021-06-15 12:55:30 +0900

    menu

---
+++ popol/resource/js/script.js
@@ -0,0 +1,25 @@
+$('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
